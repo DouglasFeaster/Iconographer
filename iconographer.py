198005560e9a1a7abcacaf6e6eeafebacfcf229f
Douglas Feaster <Douglas.Feaster@johnsonu.edu>
2024-04-22 21:11:19 -0700

    Removed old code

--- iconographer.py
+++ iconographer.py
@@ -8,15 +8,6 @@ import datetime
 sizes = [16, 24, 32, 48, 64, 128, 256]
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
-
-@click.command()
 @click.argument('src', type=click.Path(exists=True))
 # @click.argument('dst', nargs=1, type=click.Path(exists=True))
 @click.option('--ico', is_flag=True, show_default=True, default=False, type=bool, help='Generate ICO files.')
